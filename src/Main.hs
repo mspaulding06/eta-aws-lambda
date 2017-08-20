4318f2034a72e9cd4729b33f667ee0352953582d
Matt Spaulding <matt@mattops.io>
2017-08-20 15:30:45 -0700

    Fix the handler type signature

--- src/Main.hs
+++ src/Main.hs
@@ -11,13 +11,13 @@ import Java
 import Java.Collections
 
 -- | handler implements the handler function for the RequestHandler object
-handler :: (Map JString JString) -> Context -> Java (RequestHandler (Map JString JString) JString) (Map JString JString)
+handler :: (Map JString JString) -> Context -> Java (RequestHandler (Map JString JString) (Map JString JString)) (Map JString JString)
 handler m _ = return m
 
 -- | Export the handler so it can be referenced by AWS Lambda
 -- Use the reference: com.example.LambdaTest::handler
 foreign export java "handler"
-    handler :: (Map JString JString) -> Context -> Java (RequestHandler (Map JString JString) JString) (Map JString JString)
+    handler :: (Map JString JString) -> Context -> Java (RequestHandler (Map JString JString) (Map JString JString)) (Map JString JString)
 
 main :: IO ()
 main = return ()
